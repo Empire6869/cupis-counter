007048c652f539e19967b5836aeb3a266662d52d
Vladimir Gololobov <Vladimir.Gololobov@x5.ru>
2024-03-19 01:05:54 +0300

    feat(): captcha solved

--- app/reports.py
+++ app/reports.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import undetected_chromedriver as uc
 import re
+from twocaptcha import TwoCaptcha
+
+solver = TwoCaptcha("29952bd5475b49e0cbfbd26d05607ebb")
 
 
 class Reports:
@@ -18,18 +21,24 @@ class Reports:
         self.driver.find_element(By.NAME, "password").send_keys(password)
 
         # TODO: Сделать капчу
-        # img = self.driver.find_element(By.XPATH,
-        #                                '//div[@class="w-240px h-64px relative"]/img')
-        # src = img.get_attribute('src')
-        # print(src)
+        img = self.driver.find_element(By.XPATH,
+                                       '//div[@class="w-240px h-64px relative"]/img')
+        src = img.get_attribute('src')
 
-        self.driver.find_element(
-            By.XPATH, '//input[@data-qa="captcha-input"]').send_keys('123')
-        sleep(10)
-        self.driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+        try:
+            res = solver.normal(src)
+            print(res)
+            self.driver.find_element(
+                By.XPATH, '//input[@data-qa="captcha-input"]').send_keys(res["code"])
+            sleep(3)
+            self.driver.find_element(
+                By.XPATH, '//button[@type="submit"]').click()
+        except Exception as e:
+            print(e)
 
     def get_report(self, username, password):
         self.auth(username, password)
+        sleep(10)
         # self.driver.get('https://wallet.1cupis.ru/history')
         # self.driver.find_element(
         #     By.XPATH, '//button[@type="button"]').click()
